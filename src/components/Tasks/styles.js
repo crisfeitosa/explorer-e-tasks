7f5b010fd0f247c916522f91905b487012155fbd
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-06-21 12:05:26 -0300

    fix: standardize padding and gap in Container component styles

--- src/components/Tasks/styles.js
+++ src/components/Tasks/styles.js
@@ -2,11 +2,11 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   width: 100%;
-  padding: 32px;
+  padding: 2rem;
   grid-area: content;
 
   display: flex;
   flex-direction: column;
   overflow-y: scroll;
-  gap: 12px;
+  gap: 0.75rem;
 `;
