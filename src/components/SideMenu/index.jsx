f73d8ec931b2813e5d9d88c69e33fa26246495aa
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-06-21 10:50:56 -0300

    fix: update user information in SideMenu component

--- src/components/SideMenu/index.jsx
+++ src/components/SideMenu/index.jsx
@@ -17,10 +17,10 @@ export function SideMenu() {
       </Nav>
 
       <Footer>
-        <img src="https://github.com/rodrigorgtic.png" alt="Foto do usuário" />
+        <img src="https://github.com/crisfeitosa.png" alt="Foto do usuário" />
         <div>
-          <strong>Rodrigo Gonçalves</strong>
-          <small>rodrigo@email.com</small>
+          <strong>Cristiano Feitosa</strong>
+          <small>cris@email.com</small>
         </div>
 
         <Button type="button">
