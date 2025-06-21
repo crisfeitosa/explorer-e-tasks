ae45e1440c77f74de827793c6a19f59f1d85cab9
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-06-21 11:57:13 -0300

    fix: standardize font sizes and spacing in Filter component styles

--- src/components/Filter/styles.js
+++ src/components/Filter/styles.js
@@ -2,17 +2,17 @@ import styled, { css } from 'styled-components';
 
 export const Container = styled.button`
   background: none;
-  font-size: 16px;
+  font-size: 1rem;
   border: 0;
 
   color: ${({ theme }) => theme.COLORS.GRAY_100};
 
   &:after {
-    display:block;
+    display: block;
     content: '';
     border-bottom: solid 3px ${({ theme }) => theme.COLORS.BLUE_300};  
     transition: all 250ms ease-in-out;
-    padding-bottom: 16px;
+    padding-bottom: 1rem;
     transform: scaleX(0);
   }
 
